92ae301834794a91e426f16ac26c266904e815da
xiayu12345 <xiayu12345@users.noreply.github.com>
2026-09-15 09:54:52 +0000

    Explorer: stop full tree rebuild on every click (fix freeze)

--- postscript-ede79cfd.js
+++ postscript-ede79cfd.js
@@ -1,7 +1,7 @@
 await Promise.all([
   import("./static/scripts/script-0-f7c52b6a.js"),
   import("./static/scripts/script-1-ec880d15.js"),
-  import("./static/scripts/script-2-15a35ef4.js"),
+  import("./static/scripts/script-2-46438ff1.js"),
   import("./static/scripts/script-3-cc8afa9d.js"),
   import("./static/scripts/script-4-7e107d7e.js"),
   import("./static/scripts/script-5-632b2822.js"),
