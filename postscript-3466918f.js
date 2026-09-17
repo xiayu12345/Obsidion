c279e9b48c79d9eecde64fdd780ae249cbfc507a
xiayu12345 <xiayu12345@users.noreply.github.com>
2026-09-17 07:50:03 +0000

    Publish: pin 板件与项目对照 + stronger visual polish
    Explorer STRUCT_VER h133-pin-duizhao-v1; compare-panel/note-box/st-nor badges; content chrome CSS.

--- postscript-3466918f.js
+++ postscript-3466918f.js
@@ -1,7 +1,7 @@
 await Promise.all([
   import("./static/scripts/script-0-f7c52b6a.js"),
   import("./static/scripts/script-1-ec880d15.js"),
-  import("./static/scripts/script-2-5e784700.js"),
+  import("./static/scripts/script-2-0b0ad1d3.js"),
   import("./static/scripts/script-3-cc8afa9d.js"),
   import("./static/scripts/script-4-7e107d7e.js"),
   import("./static/scripts/script-5-632b2822.js"),
